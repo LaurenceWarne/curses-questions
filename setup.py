e6e4812cbedf3a3acd11901dfcbd9db07bf596ac
lWarne <laurencewarne@gmail.com>
2019-06-08 09:00:47 +0100

    Add argparse arguments to curses-questions script.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="curses questions",
+    url="",
+    version="0.1",
+    author="Laurence Warne",
+    license="MIT",
+    scripts=['bin/curses-questions'],
+    install_requires=[],
+    zip_safe=False
+)
