f99e607f62d95f1902573e19a068fcc1c6b430e4
lWarne <laurencewarne@gmail.com>
2019-07-10 10:27:12 +0100

    Use argparse in extract_anki_apkg.py, with optional argument allowing user to choose fields they want.

--- curses_questions/scripts/extract_anki_apkg.py
+++ curses_questions/scripts/extract_anki_apkg.py
@@ -1,31 +1,76 @@
 #!/usr/bin/env python3
 
+"""
+
+"""
+
+import argparse
+import re
 import sqlite3
 import sys
 import zipfile
 
 
 def main():
-    file_name = sys.argv[1]
-    with zipfile.ZipFile(file_name, "r") as zip_file:
+    description = """Extract question/answer strings and output them tab
+                     separated to stdout."""
+    parser = argparse.ArgumentParser(description=description)
+    # positional argument: infile
+    # description: apkg file to extract questions from
+    parser.add_argument(
+        "infile",
+        help="name of apkg file to extract questions from",
+    )
+    parser.add_argument(
+        "-f",
+        "--fields",
+        nargs=2,
+        help="Fields to use from the apkg file"
+    )
+    args = parser.parse_args()
+
+    with zipfile.ZipFile(args.infile, "r") as zip_file:
         zip_file.extract("collection.anki2")
-    print_db_data()
+    
+    # Sort out fields
+    if args.fields:
+        f1, f2 = args.fields
+        if not f1.isdigit() or not f2.isdigit():
+            print("Detected non-numeric input for field number!")
+            return
+        else:
+            f1, f2 = int(f1) - 1, int(f2) - 1
+            print_db_data(f1, f2)
+    else:
+        print_db_data()
 
 
-def print_db_data():
+def print_db_data(question_index=-1, answer_index=-1):
     conn = sqlite3.connect("collection.anki2")
     c = conn.cursor()
     # See:
     # https://decks.fandom.com/wiki/Anki_APKG_format_documentation
     for (content,) in c.execute("SELECT flds FROM notes"):
         parts = content.split("\x1f")
-        # Attempt to get text contents from the note field
-        text_content = [s for s in parts if  "[" not in s]
-        if len(text_content) < 2:
+        if question_index < 0 or answer_index < 0:
+            question_index = 0
+            answer_index = 1
+            # Attempt to get text contents from the note field
+            parts = [s for s in parts if  "[" not in s]
+        if len(parts) < 2:
             print("Invalid note found: " + content, file=sys.stderr)
+        elif max(question_index, answer_index) > len(parts) - 1:
+            print("Field indices out of range")
         else:
-            print(text_content[0] + "\t" + text_content[1])
+            print("{question}\t{answer}".format(
+                question=prettify_text(parts[question_index]),
+                answer=prettify_text(parts[answer_index])
+            ))
     conn.close()
+
+
+def prettify_text(text):
+    return re.sub("<.*?>", " ", text)
 
 
 if __name__ == "__main__":
